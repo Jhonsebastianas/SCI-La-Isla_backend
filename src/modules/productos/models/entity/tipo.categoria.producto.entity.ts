7c75d03216a0552ac19cf9706a50c5e2e1138d92
Jhon Sebastian <Sebastamriel@gmail.com>
2022-05-26 20:21:19 -0500

    Rollback

--- src/modules/productos/models/entity/tipo.categoria.producto.entity.ts
+++ src/modules/productos/models/entity/tipo.categoria.producto.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm'
 @Entity("TIPO_CATEGORIA_PRODUCTO")
 export class TipoCategoriaProductoEntity {
 
-    @PrimaryGeneratedColumn({ name: "ID_TIPO_CATEGORIA_PRODUCTO" })
+    @PrimaryColumn({ name: "ID_TIPO_CATEGORIA_PRODUCTO" })
     idTipoCategoriaProducto: number;
 
     @Column({ name: "NOMBRE" })
